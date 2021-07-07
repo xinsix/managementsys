a7a1f25fed7d6e1139d6bfbfe1751a488e66d361
xinsix <3426494632.com>
2021-07-07 12:08:05 +0800

    Initial commit

---
+++ src/main/java/cn/whyx/ManagementsysApplication.java
@@ -0,0 +1,13 @@
+package cn.whyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/whyx/ManagementsysApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.whyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
