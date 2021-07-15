f4b062d80c943a806655964391960e1edd36192f
xinsix <xinsix@aliyun.com>
2021-07-15 10:46:48 +0800

    update

--- src/main/java/cn/whyx/pojo/ApplyInfo.java
+++ src/main/java/cn/whyx/pojo/ApplyInfo.java
@@ -5,8 +5,7 @@ package cn.whyx.pojo;
  */
 public class ApplyInfo {
     private Integer id;//申请id
-    private Integer department_id;//部门id
-    private Integer station_id;//岗位id
+    private Integer pid;//员工id
     private int state;//状态
     private int subject;//事由（离职0or转正1）
 
@@ -18,21 +17,14 @@ public class ApplyInfo {
         this.id = id;
     }
 
-    public Integer getDepartment_id() {
-        return department_id;
+    public Integer getPid() {
+        return pid;
     }
 
-    public void setDepartment_id(Integer department_id) {
-        this.department_id = department_id;
+    public void setPid(Integer pid) {
+        this.pid = pid;
     }
 
-    public Integer getStation_id() {
-        return station_id;
-    }
-
-    public void setStation_id(Integer station_id) {
-        this.station_id = station_id;
-    }
 
     public int getState() {
         return state;
