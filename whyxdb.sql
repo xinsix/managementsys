a73afd72847894c7a2fd7eae8ff820ab5b5b6d85
xinsix <xinsix@aliyun.com>
2021-07-15 14:52:50 +0800

    更新了数据库文件

--- whyxdb.sql
+++ whyxdb.sql
@@ -16,137 +16,173 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`whyxdb` /*!40100 DEFAULT CHARACTER SET 
 
 USE `whyxdb`;
 
-/*Table structure for table `approveinnfo` */
+/*Table structure for table `applyinfo` */
 
-DROP TABLE IF EXISTS `approveinnfo`;
+DROP TABLE IF EXISTS `applyinfo`;
 
-CREATE TABLE `approveinnfo` (
+CREATE TABLE `applyinfo` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `comment` text,
-  `document` bigint(20) DEFAULT NULL,
-  `approver` bigint(20) DEFAULT NULL,
-  `approveTime` datetime DEFAULT NULL,
+  `pid` bigint(20) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `subject` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-/*Data for the table `approveinnfo` */
+/*Data for the table `applyinfo` */
 
-insert  into `approveinnfo`(`id`,`comment`,`document`,`approver`,`approveTime`) values 
-(1,'审批意见',1,1,'2021-07-07 11:30:34'),
-(2,'document',2,3,'2021-07-07 11:30:43');
+insert  into `applyinfo`(`id`,`pid`,`state`,`subject`) values 
+(1,3,0,1),
+(2,2,0,0);
 
-/*Table structure for table `document` */
+/*Table structure for table `articlesmanage` */
 
-DROP TABLE IF EXISTS `document`;
+DROP TABLE IF EXISTS `articlesmanage`;
 
-CREATE TABLE `document` (
-  `title` varchar(20) NOT NULL,
-  `description` varchar(30) DEFAULT NULL,
+CREATE TABLE `articlesmanage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `productname` varchar(20) DEFAULT NULL,
+  `applicant` varchar(20) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `applicationtime` datetime DEFAULT NULL,
+  `backtime` datetime DEFAULT NULL,
+  `purpose` varchar(255) DEFAULT NULL,
+  `record_pid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `articlesmanage` */
+
+insert  into `articlesmanage`(`id`,`productname`,`applicant`,`quantity`,`applicationtime`,`backtime`,`purpose`,`record_pid`) values 
+(1,'电脑','3',1,'2021-07-15 11:48:46','2021-07-16 11:48:50','”吃饭“',1),
+(2,'桌子','3',1,'2021-07-15 11:49:21','2021-07-16 11:49:23','吃饭',NULL);
+
+/*Table structure for table `assetsmanage` */
+
+DROP TABLE IF EXISTS `assetsmanage`;
+
+CREATE TABLE `assetsmanage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `assetname` varchar(10) DEFAULT NULL,
+  `quantity` varchar(20) DEFAULT NULL,
+  `position` varchar(30) DEFAULT NULL,
+  `assetmanager` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `assetsmanage` */
+
+insert  into `assetsmanage`(`id`,`assetname`,`quantity`,`position`,`assetmanager`) values 
+(1,'桌子','10','401',1),
+(2,'桌子','10','402',1),
+(3,'桌子','10','403',1),
+(4,'椅子','10','401',1);
+
+/*Table structure for table `attendance` */
+
+DROP TABLE IF EXISTS `attendance`;
+
+CREATE TABLE `attendance` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `creadate` datetime DEFAULT NULL,
+  `offdate` datetime DEFAULT NULL,
+  `jobib` bigint(20) DEFAULT NULL,
+  `attendancetype` int(11) DEFAULT NULL,
+  `actual` int(11) DEFAULT NULL,
+  `days` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `attendance` */
+
+insert  into `attendance`(`id`,`creadate`,`offdate`,`jobib`,`attendancetype`,`actual`,`days`) values 
+(1,'2021-07-15 08:49:15',NULL,2,0,1,22),
+(2,'2021-07-15 08:49:41',NULL,3,0,1,22),
+(3,'2021-07-15 08:50:04',NULL,4,0,1,22),
+(4,NULL,'2021-07-15 18:50:32',2,1,1,22);
+
+/*Table structure for table `carmanage` */
+
+DROP TABLE IF EXISTS `carmanage`;
+
+CREATE TABLE `carmanage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `licenseplate` varchar(10) DEFAULT NULL,
+  `applicant` varchar(20) DEFAULT NULL,
+  `applytime` datetime DEFAULT NULL,
+  `backtime` datetime DEFAULT NULL,
+  `purpose` varchar(30) DEFAULT NULL,
+  `mileage` varchar(30) DEFAULT NULL,
+  `voucher` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `carmanage` */
+
+insert  into `carmanage`(`id`,`licenseplate`,`applicant`,`applytime`,`backtime`,`purpose`,`mileage`,`voucher`) values 
+(1,'粤A-66666','3','2021-07-15 11:58:33','2021-07-16 11:58:35','吃饭','100','无'),
+(2,'粤A-55555','2','2021-07-15 11:59:08','2021-07-16 11:59:10','吃饭','10','无');
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `department` */
+
+insert  into `department`(`id`,`department_name`) values 
+(1,'财务部'),
+(2,'管理部'),
+(3,'市场部'),
+(4,'行政部'),
+(5,'开发部');
+
+/*Table structure for table `detailedprocess` */
+
+DROP TABLE IF EXISTS `detailedprocess`;
+
+CREATE TABLE `detailedprocess` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `procedure_id` bigint(20) DEFAULT NULL,
+  `matter` varchar(30) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `duration` decimal(10,1) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `apply_pid` bigint(20) DEFAULT NULL,
+  `examine_pid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `detailedprocess` */
+
+insert  into `detailedprocess`(`id`,`procedure_id`,`matter`,`start_time`,`end_time`,`duration`,`state`,`apply_pid`,`examine_pid`) values 
+(1,1,'无','2021-07-15 12:02:12','2021-07-16 12:02:15',24.0,0,3,NULL),
+(2,1,'无','2021-07-15 12:03:48','2021-07-16 12:03:50',24.0,1,2,NULL);
+
+/*Table structure for table `infomanage` */
+
+DROP TABLE IF EXISTS `infomanage`;
+
+CREATE TABLE `infomanage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `content` text,
-  `creator` bigint(20) DEFAULT NULL,
-  `createtime` datetime DEFAULT NULL,
-  `workflow` varchar(10) DEFAULT NULL,
-  `processed` varchar(1) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `document` */
-
-insert  into `document`(`title`,`description`,`content`,`creator`,`createtime`,`workflow`,`processed`,`status`) values 
-('文档标题','文档描述','文档内容',1,'2021-07-07 10:47:40','未知','0','0'),
-('文档标题1','文档描述','文档内容2',2,'2021-07-07 10:48:42','未知','1','1');
-
-/*Table structure for table `fieidinput` */
-
-DROP TABLE IF EXISTS `fieidinput`;
-
-CREATE TABLE `fieidinput` (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL,
-  `template` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-/*Data for the table `fieidinput` */
-
-insert  into `fieidinput`(`id`,`name`,`template`) values 
-(1,'表单1','表单1模板'),
-(2,'表单2','表单2模板'),
-(3,'表单3','表单3模板');
-
-/*Table structure for table `fieiditem` */
-
-DROP TABLE IF EXISTS `fieiditem`;
-
-CREATE TABLE `fieiditem` (
-  `label` varchar(10) NOT NULL,
-  `value` text,
-  PRIMARY KEY (`label`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Data for the table `fieiditem` */
-
-insert  into `fieiditem`(`label`,`value`) values 
-('域项目标签1','域项目值1'),
-('域项目标签2','域项目值2\r\n');
-
-/*Table structure for table `fieidtype` */
-
-DROP TABLE IF EXISTS `fieidtype`;
-
-CREATE TABLE `fieidtype` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL,
-  `type` varchar(20) DEFAULT NULL,
+  `releasetime` datetime DEFAULT NULL,
+  `revisiontime` datetime DEFAULT NULL,
+  `release_pid` varchar(20) DEFAULT NULL,
+  `modify_pid` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-/*Data for the table `fieidtype` */
+/*Data for the table `infomanage` */
 
-insert  into `fieidtype`(`id`,`name`,`type`) values 
-(1,'域类型名称1','类型1'),
-(2,'域类型名称2','类型2');
-
-/*Table structure for table `flowform` */
-
-DROP TABLE IF EXISTS `flowform`;
-
-CREATE TABLE `flowform` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `workflow` bigint(20) DEFAULT NULL,
-  `template` text,
-  `fieId` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-/*Data for the table `flowform` */
-
-insert  into `flowform`(`id`,`workflow`,`template`,`fieId`) values 
-(1,1,'模板1\r\n',1),
-(2,2,'模板2\r\n',2),
-(3,3,'模板3',3);
-
-/*Table structure for table `formfieid` */
-
-DROP TABLE IF EXISTS `formfieid`;
-
-CREATE TABLE `formfieid` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `fieIdLabel` varchar(10) DEFAULT NULL,
-  `fieIdname` varchar(10) DEFAULT NULL,
-  `fieIdInput` varchar(10) DEFAULT NULL,
-  `flowform` bigint(20) DEFAULT NULL,
-  `items` text,
-  `fieIdtype` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-/*Data for the table `formfieid` */
-
-insert  into `formfieid`(`id`,`fieIdLabel`,`fieIdname`,`fieIdInput`,`flowform`,`items`,`fieIdtype`) values 
-(1,'表单域标签1','表单域的名称1','表单域的输入形式',1,'表单域集合1','表单域类型'),
-(2,'表单域标签2','表单域的名称2','表单域的输入形式',2,'表单域集合2','表单域类型');
+insert  into `infomanage`(`id`,`content`,`releasetime`,`revisiontime`,`release_pid`,`modify_pid`) values 
+(1,'紧急通知','2021-07-15 11:43:58',NULL,'1',NULL),
+(2,'红红火火恍恍惚惚或或','2021-07-15 11:44:26',NULL,'2',NULL);
 
 /*Table structure for table `meetingroom` */
 
@@ -188,48 +224,45 @@ insert  into `meetingroomapply`(`id`,`meetroomid`,`begindate`,`enddate`,`applyre
 (2,2,'2021-07-07 10:51:01','2021-07-07 13:51:05','开会','1'),
 (3,3,'2021-07-07 10:51:22','2021-07-07 11:51:24','紧急会议','2');
 
-/*Table structure for table `module` */
+/*Table structure for table `note` */
 
-DROP TABLE IF EXISTS `module`;
+DROP TABLE IF EXISTS `note`;
 
-CREATE TABLE `module` (
-  `id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL,
-  `sn` varchar(10) DEFAULT NULL,
-  `url` varchar(10) DEFAULT NULL,
-  `orderNo` varchar(10) DEFAULT NULL,
-  `parent` bigint(20) DEFAULT NULL,
-  `chidren` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-/*Data for the table `module` */
-
-insert  into `module`(`id`,`name`,`sn`,`url`,`orderNo`,`parent`,`chidren`) values 
-(1,'模块1','模块SN','模块地址','模块排序号',NULL,2),
-(2,'模块名称2','模块SN2','模块地址2','模块排序号',1,3),
-(3,'模块3','模块SN3','模块地址3','模块排序号',2,NULL);
-
-/*Table structure for table `organization` */
-
-DROP TABLE IF EXISTS `organization`;
-
-CREATE TABLE `organization` (
+CREATE TABLE `note` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL,
-  `sn` varchar(20) DEFAULT NULL,
-  `description` varchar(30) DEFAULT NULL,
-  `parent` bigint(20) DEFAULT NULL,
-  `chiIdren` bigint(20) DEFAULT NULL,
+  `notecontent` varchar(20) DEFAULT NULL,
+  `executiontime` datetime DEFAULT NULL,
+  `creationtime` datetime DEFAULT NULL,
+  `revisiontime` datetime DEFAULT NULL,
+  `employeeid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-/*Data for the table `organization` */
+/*Data for the table `note` */
 
-insert  into `organization`(`id`,`name`,`sn`,`description`,`parent`,`chiIdren`) values 
-(1,'机构名称','机构SN','机构描述',NULL,2),
-(2,'机构名称2','机构SN2','机构描述2',1,3),
-(3,'机构名称3','机构SN3','机构描述3',2,NULL);
+insert  into `note`(`id`,`notecontent`,`executiontime`,`creationtime`,`revisiontime`,`employeeid`) values 
+(1,'哈哈哈','2021-07-15 11:37:25','2021-07-15 11:37:30',NULL,1),
+(2,'哈哈哈哈','2021-07-15 11:37:41','2021-07-15 11:37:43',NULL,2),
+(3,'哈哈哈哈哈','2021-07-15 11:37:50','2021-07-15 11:37:53',NULL,3);
+
+/*Table structure for table `organmanage` */
+
+DROP TABLE IF EXISTS `organmanage`;
+
+CREATE TABLE `organmanage` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `departmentid` bigint(20) DEFAULT NULL,
+  `postid` bigint(20) DEFAULT NULL,
+  `position` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `organmanage` */
+
+insert  into `organmanage`(`id`,`departmentid`,`postid`,`position`) values 
+(1,1,1,'实习员工'),
+(2,1,2,'普通员工 '),
+(3,1,3,'组长');
 
 /*Table structure for table `penson` */
 
@@ -238,21 +271,41 @@ DROP TABLE IF EXISTS `penson`;
 CREATE TABLE `penson` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
   `sex` varchar(1) DEFAULT NULL,
   `address` varchar(30) DEFAULT NULL,
-  `duty` varchar(10) DEFAULT NULL,
   `phone` varchar(11) DEFAULT NULL,
   `description` varchar(30) DEFAULT NULL,
-  `org` varchar(10) DEFAULT NULL,
+  `org` bigint(20) DEFAULT NULL,
+  `role` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 /*Data for the table `penson` */
 
-insert  into `penson`(`id`,`name`,`sex`,`address`,`duty`,`phone`,`description`,`org`) values 
-(1,'张三','男','广东佛山','员工','12345678901','无','财务'),
-(2,'李四','女','广东广州','员工','12345678912','无','行政'),
-(3,'王五','女','广东清远','员工','12332112345','无','人事');
+insert  into `penson`(`id`,`name`,`username`,`password`,`sex`,`address`,`phone`,`description`,`org`,`role`) values 
+(1,'保密','admin','admin','0','中国','11111111111',NULL,NULL,1),
+(2,'张三','zhangsan','123456','1','广东','22222222222',NULL,1,1),
+(3,'李四','lisi','123456','0','广西','33333333333',NULL,2,7),
+(4,'王五','wangwu','123456','1','湖南','55555555555',NULL,3,7);
+
+/*Table structure for table `procedure` */
+
+DROP TABLE IF EXISTS `procedure`;
+
+CREATE TABLE `procedure` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `procedure` */
+
+insert  into `procedure`(`id`,`name`) values 
+(1,'请假'),
+(2,'加班'),
+(3,'出差');
 
 /*Table structure for table `role` */
 
@@ -262,77 +315,59 @@ CREATE TABLE `role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 /*Data for the table `role` */
 
 insert  into `role`(`id`,`name`) values 
-(1,'系统管理员'),
-(2,'人事部门管理'),
-(3,'行政部门管理'),
-(4,'业务部门管理'),
-(5,'普通员工');
+(1,'超级管理员'),
+(2,'财务部部长'),
+(3,'行政部部长'),
+(4,'开发部部长'),
+(5,'市场部部长'),
+(6,'管理部部长'),
+(7,'员工');
 
-/*Table structure for table `user` */
+/*Table structure for table `station` */
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `station`;
 
-CREATE TABLE `user` (
+CREATE TABLE `station` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(10) DEFAULT NULL,
-  `password` varchar(10) DEFAULT NULL,
-  `createTime` datetime DEFAULT NULL,
-  `expireTime` datetime DEFAULT NULL,
-  `parson` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
-
-/*Data for the table `user` */
-
-insert  into `user`(`id`,`username`,`password`,`createTime`,`expireTime`,`parson`) values 
-(1,'zhangsan','123456','2021-07-07 10:39:40','2022-07-07 10:39:46',1),
-(2,'lisi','123456','2021-07-07 10:40:13','2022-07-07 10:40:15',2),
-(3,'wangwu','123456','2021-07-07 10:40:30','2022-07-07 10:40:32',3),
-(4,'admin','admin','2021-07-07 11:27:05','2029-07-07 11:27:07',NULL),
-(5,'xinsix','xinsix','2021-07-07 11:28:06','2022-07-07 11:28:08',4);
-
-/*Table structure for table `usersroles` */
-
-DROP TABLE IF EXISTS `usersroles`;
-
-CREATE TABLE `usersroles` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `role` bigint(20) DEFAULT NULL,
-  `user` bigint(20) DEFAULT NULL,
-  `orderNo` bigint(20) DEFAULT NULL,
+  `station_name` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-/*Data for the table `usersroles` */
+/*Data for the table `station` */
 
-insert  into `usersroles`(`id`,`role`,`user`,`orderNo`) values 
-(1,2,3,3),
-(2,3,2,2),
-(3,1,4,1),
-(4,5,5,5);
+insert  into `station`(`id`,`station_name`) values 
+(1,'总会计师'),
+(2,'出纳'),
+(3,'稽核'),
+(4,'核算');
 
-/*Table structure for table `workflow` */
+/*Table structure for table `task` */
 
-DROP TABLE IF EXISTS `workflow`;
+DROP TABLE IF EXISTS `task`;
 
-CREATE TABLE `workflow` (
+CREATE TABLE `task` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(10) DEFAULT NULL,
-  `processDefinition` text,
-  `processImge` text,
+  `matter` varchar(20) DEFAULT NULL,
+  `starttime` datetime DEFAULT NULL,
+  `endtime` datetime DEFAULT NULL,
+  `creationtime` datetime DEFAULT NULL,
+  `revisiontime` datetime DEFAULT NULL,
+  `state` int(255) DEFAULT NULL,
+  `employeeid` bigint(20) DEFAULT NULL,
+  `executeid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-/*Data for the table `workflow` */
+/*Data for the table `task` */
 
-insert  into `workflow`(`id`,`name`,`processDefinition`,`processImge`) values 
-(1,'工作流名称1','流程文件定义','??'),
-(2,'工作流名称2','流程文件定义','??');
+insert  into `task`(`id`,`matter`,`starttime`,`endtime`,`creationtime`,`revisiontime`,`state`,`employeeid`,`executeid`) values 
+(1,'哈哈哈哈','2021-07-15 11:38:27','2021-08-07 11:41:33','2021-07-15 11:38:30',NULL,0,3,1),
+(2,'哈哈哈哈哈哈','2021-07-15 11:41:47','2021-07-16 11:41:49','2021-07-15 11:41:51',NULL,0,2,1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
